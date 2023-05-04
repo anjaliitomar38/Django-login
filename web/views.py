6ac3d1eadd90a8de8b08c4ad8a1039fa06cd6edd
anjaliitomar38 <anjali.tomar@cubexo.io>
2023-05-04 10:59:07 +0530

    Added all files

---
+++ web/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect
+from .models import Member
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        member = Member(username=request.POST['username'], password=request.POST['password'],  firstname=request.POST['firstname'], lastname=request.POST['lastname'])
+        member.save()
+        return redirect('/')
+    else:
+        return render(request, 'web/index.html')
+
+def login(request):
+    return render(request, 'web/login.html')
+
+def home(request):
+    if request.method == 'POST':
+        if Member.objects.filter(username=request.POST['username'], password=request.POST['password']).exists():
+            member = Member.objects.get(username=request.POST['username'], password=request.POST['password'])
+            return render(request, 'web/home.html', {'member': member})
+        else:
+            context = {'msg': 'Invalid username or password'}
+            return render(request, 'web/login.html', context)
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
